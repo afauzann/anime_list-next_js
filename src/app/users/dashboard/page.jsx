ce9bc6f59c9ff6c6277bd8148d49c6164f3e02e9
afauzann <afauzan857@gmail.com>
2023-12-25 12:17:53 +0700

    Middleware & Dashboard User

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,14 +1,28 @@
 import { authUserSession } from '@/libs/auth-libs'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default async function Page() {
     const user = await authUserSession()
 
     return (
-        <div className="text-color-primary">
-            <h3>Dashboard</h3>
-            <h5>Welcome... {user.name}</h5>
-            <Image src={user.image} alt="..." width={250} height={250}/>
+        <div className="text-color-primary flex flex-col mt-8 justify-center items-center">
+            <h5 className='text-2xl font-bold'>Welcome, {user?.name}</h5>
+            <Image src={user?.image} alt="..." width={250} height={250}/>
+            <div className='flex flex-wrap py-8 gap-4'>
+                <Link 
+                    className='bg-color-accent text-color-dark font-bold py-3 px-4 rounded text-xl'
+                    href="/users/dashboard/collection"
+                >
+                    My Collection
+                </Link>
+                <Link 
+                    className='bg-color-accent text-color-dark font-bold py-3 px-4 rounded text-xl'
+                    href="/users/dashboard/comment"
+                >
+                    My Comment
+                </Link>
+            </div>
         </div>
     )
 }
